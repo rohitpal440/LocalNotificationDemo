7d96ee4fe20ae7d93850d1acc7fcf6f37e8de3fb
Rohit Pal <rohitpal440@gmail.com>
2016-08-11 22:22:39 +0530

    Initial Commit

---
+++ Local Notification Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Local Notification Demo
+//
+//  Created by Rohit Pal on 11/08/16.
+//  Copyright © 2016 Rohit Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
