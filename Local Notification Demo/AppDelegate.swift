1ca5bc24b71f78a10121b3d05f032d6d7cc059ac
Rohit Pal <rohitpal440@gmail.com>
2016-08-12 00:15:24 +0530

    Adding foreground and background notification handeling

--- Local Notification Demo/AppDelegate.swift
+++ Local Notification Demo/AppDelegate.swift
@@ -16,9 +16,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let tintColor = UIColor(red: 252/255, green: 72/255, blue: 49/255, alpha: 1)
+        window!.tintColor = tintColor
+        
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes:[.Alert,.Badge,.Sound], categories: nil))
         return true
     }
 
+    func application(application:UIApplication, didReceiveLocalNotification notification:UILocalNotification){
+        if application.applicationState == .Active{
+            self.takeActiveActionNotification(notification)
+        } else{
+            self.takeInactiveActionNotification(notification)
+        }
+    }
+    
+    func takeActiveActionNotification(localNotification: UILocalNotification) {
+        let notificationMessage =  localNotification.userInfo!["message"] as! String
+        let alertController = UIAlertController(title: "Hey there!", message: notificationMessage, preferredStyle: .Alert)
+        let remindMeLaterAction = UIAlertAction(title: "Remind me later!", style: .Default, handler: nil)
+        let sureAction = UIAlertAction(title: "Sure", style: .Default) { (action) -> Void in
+             print("User is interested in notification")
+        }
+        alertController.addAction(remindMeLaterAction)
+        alertController.addAction(sureAction)
+        self.window?.rootViewController?.presentViewController(alertController , animated: true, completion: nil)
+    }
+    
+    
+    func takeInactiveActionNotification(localNotification: UILocalNotification) {
+        
+        var AlertView = UIAlertController(title: "Alert", message: "You pushed me to background! Here! You are", preferredStyle: UIAlertControllerStyle.Alert)
+        AlertView.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:nil))
+        self.window?.rootViewController?.presentViewController(AlertView, animated: true, completion: nil)
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- Local Notification Demo/ViewController.swift
+++ Local Notification Demo/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var time = 5
+    var timer = NSTimer()
+    
+    @IBOutlet weak var notificationButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: Selector("notification"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func notification(){
+        time -= 1
+        if time == 0 {
+            var notification = UILocalNotification()
+            notification.alertAction = "Open App"
+            notification.alertBody = "You have an Local Notification"
+            notification.applicationIconBadgeNumber = 1
+            notification.soundName = UILocalNotificationDefaultSoundName
+            notification.userInfo = [
+                "message": "Checkout New notification From Local Notification Demo App" 
+            ]
+            notification.fireDate = NSDate(timeIntervalSinceNow: 0)
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            
+            timer.invalidate()
+        }
+        
+    }
+
+    @IBAction func createNotification(sender: AnyObject) {
+        var AlertView = UIAlertController(title: "Alert", message: "You Got Notification!", preferredStyle: UIAlertControllerStyle.Alert)
+        AlertView.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler:nil))
+        self.presentViewController(AlertView, animated: true, completion: nil)
+        
+    }
 
 }
 
